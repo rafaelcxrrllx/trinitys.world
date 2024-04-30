cb37a4fdc0709ba0d24327c218fa9b54f91541b5
Rafael Carrillo Muñoz <carrilr4@uci.edu>
2024-04-30 15:33:11 -0700

    Need to readjust the planets.

--- project-trinity/src/Haven.js
+++ project-trinity/src/Haven.js
@@ -2,23 +2,58 @@ import React from 'react';
 import "./styles.css"
 import "./Haven.scss"
 import {motion} from "framer-motion";
+import moon from './assets/planets/moon.svg';
+import sol from './assets/planets/sol.svg';
+import mars from './assets/planets/mars.svg';
+import venus from './assets/planets/venus.svg';
+import jupiter from './assets/planets/jupiter.svg';
+import saturn from './assets/planets/saturn.svg';
+import neptune from './assets/planets/neptune.svg';
+import uranus from './assets/planets/uranus.svg';
+import pluto from './assets/planets/pluto.svg';
+import mercury from './assets/planets/mercury.svg';
+
 
 function Haven() {
 
   
     return (
-    	<div class="Haven-container">
-			{/* <!-- orbits --> */}
-			<div class="orbit orbit-1"></div>
-			<div class="orbit orbit-2"></div>
-			<div class="orbit orbit-3"></div>
-			<div class="orbit orbit-4"></div>
-			<div class="orbit orbit-5"></div>
-			<div class="orbit orbit-6"></div>
-			<div class="orbit orbit-7"></div>
-			<div class="orbit orbit-8"></div>
-			<div class="orbit orbit-9"></div> 
+		<div class="Heaven-container">
+			<div class="Haven-container">
+				{/* <!-- orbits --> */}
+				<div class="orbit orbit-1">
+					<img src={moon} />
+				</div>
+				<div class="orbit orbit-2">
+				<img src={mercury} />
+				</div>
+				<div class="orbit orbit-3">
+				<img src={venus} />
+				</div>
+				<div class="orbit orbit-4">
+				<img src={sol} />
+				</div>
+				<div class="orbit orbit-5">
+				<img src={mars} />
+				</div>
+				<div class="orbit orbit-6">
+				<img src={jupiter} />
+				</div>
+				<div class="orbit orbit-7">
+				<img src={saturn} />
+				</div>
+				<div class="orbit orbit-8">
+				<img src={uranus} />
+				</div>
+				<div class="orbit orbit-9">
+				<img src={neptune} />
+				</div> 
+				<div class="orbit orbit-9">
+				<img src={pluto} />
+				</div> 
+			</div>
 		</div>
+		
 
     );
 }
